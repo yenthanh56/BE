7423a203634c47babfff18ea790cc216d4929bfb
Thanh Ngo <yenthanh5697@gmail.com>
2023-01-04 10:57:58 +0700

    fix

--- app.js
+++ app.js
@@ -42,21 +42,16 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Credentials", true);
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Credentials", true);
+// 	next();
+// });
 app.use(express.json());
-app.use(
-	cors({
-		origin: "https://tiki-app-alpha.vercel.app/",
-	})
-);
+app.use(cors());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
 app.use(cookieParser());
 
 mongoose.connect(
