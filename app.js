e30326591d248008072138b4a6bacd3fc0fb30e5
yenthanh56 <57222518+yenthanh56@users.noreply.github.com>
2023-02-27 09:59:01 +0700

    update

--- app.js
+++ app.js
@@ -43,37 +43,28 @@ app.use(
 app.use(function (req, res, next) {
 	// Website you wish to allow to connect
 	res.setHeader("Access-Control-Allow-Origin", "*");
-
-	// Request methods you wish to allow
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
 	res.setHeader(
 		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+		"PUT, POST, GET, DELETE, PATCH, OPTIONS"
 	);
-
-	// Request headers you wish to allow
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With,content-type"
-	);
-
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader("Access-Control-Allow-Credentials", true);
 
 	// Pass to next layer of middleware
 	next();
 });
-const corsOptions = {
-	origin: "https://storeapp-beta.vercel.app/",
-	credentials: true,
-	optionSuccessStatus: 200,
-	methods: ["GET", "POST", "PUT", "DELETE"],
-};
+// const corsOptions = {
+// 	origin: "https://storeapp-beta.vercel.app/",
+// 	credentials: true,
+// 	optionSuccessStatus: 200,
+// 	methods: ["GET", "POST", "PUT", "DELETE"],
+// };
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/v1/userauth", userRouter);
 app.use("/v1/dealhot", dealRouter);
