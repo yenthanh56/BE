c28511ddd0fcec599ac1c6e503bde0067225fa57
yenthanh56 <57222518+yenthanh56@users.noreply.github.com>
2023-02-27 08:17:29 +0700

    update

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 	next();
 });
 const corsOptions = {
-	origin: "http://localhost:3000",
+	origin: "https://storeapp-beta.vercel.app/",
 	credentials: true,
 	optionSuccessStatus: 200,
 };
