2435e231267cdbe9034e962aa0bf839d56a280c9
yenthanh56 <57222518+yenthanh56@users.noreply.github.com>
2023-02-27 08:30:11 +0700

    update

--- app.js
+++ app.js
@@ -40,8 +40,15 @@ app.use(
 		maxAge: 24 * 60 * 60 * 100,
 	})
 );
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Credentials", true);
+app.use(function (req, res, next) {
+	//Enabling CORS
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+	);
+
 	next();
 });
 const corsOptions = {
